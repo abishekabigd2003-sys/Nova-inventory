6e6a39145646cd4c156a90b15d572ae2d04335c9
abishekabigd2003 <abishekabigd2003@gmail.com>
2026-07-08 18:23:59 +0530

    Fix Stock In API relative path issue causing syntax errors on production

--- inventory-management-system/frontend/src/api/stockin.api.js
+++ inventory-management-system/frontend/src/api/stockin.api.js
@@ -1,61 +1,28 @@
+import api from './api';
+
 const STOCKIN_URL = '/api/stockin';
 
-const getHeaders = () => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  };
-  const stored = localStorage.getItem('ims_user');
-  if (stored) {
-    try {
-      const { token } = JSON.parse(stored);
-      if (token) headers['Authorization'] = `Bearer ${token}`;
-    } catch (e) {}
-  }
-  return headers;
-};
-
-const handleResponse = async (res) => {
-  const data = await res.json();
-  if (!res.ok) {
-    throw { response: { data } }; // Mock Axios error format
-  }
-  return data;
-};
-
 export const getStockInRecords = async (params = {}) => {
-  const query = new URLSearchParams(params).toString();
-  const res = await fetch(`${STOCKIN_URL}?${query}`, { headers: getHeaders() });
-  return handleResponse(res);
+  const res = await api.get(STOCKIN_URL, { params });
+  return res.data;
 };
 
 export const getStockInRecord = async (id) => {
-  const res = await fetch(`${STOCKIN_URL}/${id}`, { headers: getHeaders() });
-  return handleResponse(res);
+  const res = await api.get(`${STOCKIN_URL}/${id}`);
+  return res.data;
 };
 
 export const createStockInRecord = async (recordData) => {
-  const res = await fetch(STOCKIN_URL, {
-    method: 'POST',
-    headers: getHeaders(),
-    body: JSON.stringify(recordData)
-  });
-  return handleResponse(res);
+  const res = await api.post(STOCKIN_URL, recordData);
+  return res.data;
 };
 
 export const updateStockInRecord = async (id, recordData) => {
-  const res = await fetch(`${STOCKIN_URL}/${id}`, {
-    method: 'PUT',
-    headers: getHeaders(),
-    body: JSON.stringify(recordData)
-  });
-  return handleResponse(res);
+  const res = await api.put(`${STOCKIN_URL}/${id}`, recordData);
+  return res.data;
 };
 
 export const deleteStockInRecord = async (id) => {
-  const res = await fetch(`${STOCKIN_URL}/${id}`, {
-    method: 'DELETE',
-    headers: getHeaders()
-  });
-  return handleResponse(res);
+  const res = await api.delete(`${STOCKIN_URL}/${id}`);
+  return res.data;
 };
